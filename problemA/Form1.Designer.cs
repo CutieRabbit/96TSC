d6b2bcf45b0dd28e0ee8f8cef5f8b8862bb6593a
Xuan <52220077+CutieRabbit@users.noreply.github.com>
2019-07-04 15:25:57 +0800

    problemA uplode

---
+++ problemA/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace problemA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "身分證字號";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(119, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(84, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(119, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "出生年月日";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "婚姻";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "未婚",
+            "已婚"});
+            this.comboBox1.Location = new System.Drawing.Point(84, 70);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(86, 122);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(119, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "手機號碼";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "編碼";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(109, 159);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "解碼";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 194);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 12);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "10 Bytes Output";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 209);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(189, 22);
+            this.textBox4.TabIndex = 11;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "博士",
+            "碩士",
+            "大學",
+            "專科",
+            "高中",
+            "國中",
+            "國小",
+            "未知"});
+            this.comboBox2.Location = new System.Drawing.Point(86, 96);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(119, 20);
+            this.comboBox2.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 99);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "學歷";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 401);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ problemA/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace problemA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        public string converter(string str)
+        {
+            string result = "";
+            for(int i = 0; i < 20; i++)
+            {
+                result += Convert.ToString(Convert.ToInt16(str.Substring(4 * i, 4),2),16);
+            }
+            return result;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string result1 = "";
+            string result2 = "";
+            string result3 = "";
+            string result4 = "";
+            string data1 = textBox1.Text;
+            string data2 = textBox2.Text;
+            string data3 = comboBox1.Text;
+            string data4 = comboBox2.Text;
+            string data5 = textBox3.Text;
+            result1 = Convert.ToString(Convert.ToInt64(data1.Substring(2)),2).PadLeft(27,'0');
+            result1 = (data1[1].Equals('1') ? '0' : '1') + result1;
+            result1 = Convert.ToString(0 + (data1[0] - 'A'), 2).PadLeft(5,'0')+ result1;
+            result1.PadLeft(32, '0');
+
+            string year = Convert.ToString(Convert.ToInt16(data2.Substring(0, 4)) - 1900, 2).PadLeft(7,'0');
+            string month = Convert.ToString(Convert.ToInt16(data2.Substring(4, 2)),2).PadLeft(4,'0');
+            string day = Convert.ToString(Convert.ToInt16(data2.Substring(6, 2)), 2).PadLeft(5, '0');
+            result2 = year + month + day;
+            result2 = comboBox1.Items.IndexOf(data3) + result2;
+
+            result3 = Convert.ToString(comboBox2.Items.IndexOf(data4),2).PadLeft(3,'0');
+            result4 = Convert.ToString(Convert.ToInt32(data5.Substring(2)),2).PadLeft(27,'0');
+
+            string result = result4 + " " + result3 + " " + result2 + " " + result1;
+
+            Console.WriteLine(result);
+
+            textBox4.Text = converter(result.Replace(" ","")).ToUpper();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string data = textBox4.Text;
+            string str = "";
+            for(int i = 0; i < data.Length; i++)
+            {
+                str += Convert.ToString(Convert.ToInt16(data[i].ToString(),16),2).PadLeft(4,'0');
+            }
+            Console.WriteLine(str);
+            string phoneNumber = "09" + Convert.ToInt32(str.Substring(0, 27),2);
+            string study = comboBox2.Items[Convert.ToInt32(str.Substring(27, 3), 2)].ToString();
+            string married = comboBox1.Items[Convert.ToInt32(str[30].ToString(), 2)].ToString();
+            string year = (Convert.ToInt32(str.Substring(31, 7), 2) + 1900).ToString();
+            string month = Convert.ToInt32(str.Substring(38, 4), 2).ToString().PadLeft(2,'0');
+            string day = Convert.ToInt32(str.Substring(42, 5), 2).ToString().PadLeft(2, '0');
+            string word = "" + ((char)(Convert.ToInt32(str.Substring(47, 5), 2) + 'A')).ToString();
+            string number1 = (Convert.ToInt32(str.Substring(52, 1), 2)+1).ToString();
+            string number2 = (Convert.ToInt32(str.Substring(53),2)).ToString();
+            textBox1.Text = word + number1 + number2;
+            textBox2.Text = year + month + day;
+            comboBox2.Text = study;
+            textBox3.Text = phoneNumber;
+            comboBox1.Text = married;
+        }
+    }
+}
